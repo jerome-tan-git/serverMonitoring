dd7a57d7b9c9e65b0eb78b7a1cf5a16a2b6d42cb
jerome-tan-git <13671605961@163.com>
2015-06-03 11:57:17 +0800

    成功测试future, actor里面要这么写：this.getSender().tell("hello world", this.getSelf()); 不然会超时

--- src/main/java/com/jerome/validate/StartValidate.java
+++ src/main/java/com/jerome/validate/StartValidate.java
@@ -1,11 +1,11 @@
 package com.jerome.validate;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import scala.concurrent.Await;
+import scala.concurrent.ExecutionContext;
 import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
@@ -16,6 +16,7 @@ import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.dispatch.Futures;
 import akka.dispatch.Mapper;
+import akka.dispatch.OnComplete;
 import akka.dispatch.OnSuccess;
 import akka.pattern.Patterns;
 import akka.routing.RoundRobinPool;
@@ -27,46 +28,130 @@ public class StartValidate {
 	private Config conf;
 	private Timeout timeout;
 	private ActorSystem system;
-	public StartValidate(Config _conf)
-	{
-		 system = ActorSystem.create("test");
-		 this.conf = _conf;
-		 child = system.actorOf(new RoundRobinPool(this.conf.getThreadCount()).props(Props.create(Worker.class)), "works");
-		 timeout = new Timeout(Duration.create(5, "seconds"));
+
+	public StartValidate(Config _conf) {
+		system = ActorSystem.create("test");
+		this.conf = _conf;
+		child = system.actorOf(new RoundRobinPool(this.conf.getThreadCount())
+				.props(Props.create(Worker.class)), "works");
+		timeout = new Timeout(Duration.create(3, "seconds"));
 	}
-	
-	public void startValidate()
-	{
+
+	public void startValidate() throws Exception {
+		ExecutionContext ec = system.dispatcher();
 		List<Future<String>> frs = new ArrayList<Future<String>>();
-		Future f1 = akka.pattern.Patterns.ask(child, "1,50000", timeout);
-		Future f2 = akka.pattern.Patterns.ask(child, "~~~", timeout);
-		frs.add(f1);
-        frs.add(f2);
-        Future<Iterable<String>> future = Futures.sequence(frs, this.system.dispatcher());
-        Future<Double> fr = future.map(arg0, arg1)
+		for (int i = 0; i < 10; i++) {
+			Future f1 = akka.pattern.Patterns.ask(child, "msg: " + i, timeout);
+			frs.add(f1);
+		}
+		Future<Iterable<String>> future = Futures.sequence(frs,
+				this.system.dispatcher());
 
+		Future<String> fr = future.map(new Mapper<Iterable<String>, String>() {
+			@Override
+			public String apply(Iterable<String> parameter) {
+				String result = "";
+				for (String s : parameter) {
+					System.out.println("-->: " + s);
+					result += s + " ";
+				}
+				return result;
+			}
+		}, ec);
+		System.out.println(Await.result(fr, Duration.create(10, "s")));
+		fr.onSuccess(new OnSuccess<String>() {
+            @Override
+            public void onSuccess(String result) {
+                System.out.println("f2·µ»Ø½á¹û:" + result);
+            }
+        }, ec);
+		
+		 try {
+		 Thread.sleep(5000);
+		 } catch (InterruptedException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 }
+		// ExecutionContext ec = system.dispatcher();
+		// for(int i=0;i<1;i++)
+		// {
+		// Future f = Patterns.ask(child, "message: " + i, timeout);
+		// frs.add(f);
+		// }
+		// System.out.println("x");
+		// Future<Iterable<String>> future = Futures.sequence(frs,
+		// this.system.dispatcher());
+		// Future<String> fr = future.map(new Mapper<Iterable<String>, String>()
+		// {
+		// @Override
+		// public String apply(Iterable<String> parameter) {
+		// String result = "";
+		// for (String s : parameter) {
+		// System.out.println("-->: " + s);
+		// result += s + " ";
+		// }
+		// return result;
+		// }
+		// }, ec);
+		//
+		//
+		//
+		// try {
+		// System.out.println(Await.result(fr, Duration.create(10, "s")));
+		// } catch (Exception e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 
-    }
-//		 Future<Object> future = Patterns.ask(child, "are you ready?", timeout);
-//		 String result= null;
-//		try {
-//			result = (String) Await.result(future, timeout.duration());
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-	     System.out.println(result);
-	     this.shutdown();
+		// try {
+		// Await.result(frs, Duration.create(20, "s"));
+		// } catch (Exception e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// try {
+		// Thread.sleep(10000);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// Future f1 = akka.pattern.Patterns.ask(child, "1,50000", timeout);
+		// Future f2 = akka.pattern.Patterns.ask(child, "~~~", timeout);
+		// frs.add(f1);
+		// frs.add(f2);
+		// Future<Iterable<String>> future = Futures.sequence(frs,
+		// this.system.dispatcher());
+		// try {
+		// Await.result(future, Duration.create(0, "s"));
+		// } catch (Exception e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// Future<Double> fr = future.map(arg0, arg1)
+
+		// Future<Object> future = Patterns.ask(child, "are you ready?",
+		// timeout);
+		// String result= null;
+		// try {
+		// result = (String) Await.result(future, timeout.duration());
+		// } catch (Exception e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// System.out.println(result);
+		this.shutdown();
 	}
-	
-	
-	public void shutdown()
-	{
+
+	public void shutdown() {
 		this.system.shutdown();
 	}
-	public static void main(String[] args) throws IOException
-	{
-		StartValidate validator = new StartValidate(ReadConfig.readConfig("./config.json"));
+
+	public static void main(String[] args) throws Exception {
+		StartValidate validator = new StartValidate(
+				ReadConfig.readConfig("./config.json"));
 		validator.startValidate();
 	}
 }
--- src/main/java/com/jerome/validate/Worker.java
+++ src/main/java/com/jerome/validate/Worker.java
@@ -7,14 +7,14 @@ public class Worker extends UntypedActor{
 	@Override
 	public void onReceive(Object _msg) throws Exception {
 		if (_msg instanceof String) {
-            Thread.sleep(3000);
-            System.out.println(Thread.currentThread().getName() + " sleep end");
+            Thread.sleep(1000);
+//            System.out.println(Thread.currentThread().getName() + " sleep end");
             System.out.println("接收的消息：" + _msg);
             // 返回一个消息
             this.getSender().tell("hello world", this.getSelf());
-            System.out.println("sender path=" + this.getSender().path());
-            getContext().stop(this.getSelf());
-            System.out.println("|||{} has stop : " + this.getSelf().path());
+//            System.out.println("sender path=" + this.getSender().path());
+//            getContext().stop(this.getSelf());
+//            System.out.println("|||{} has stop : " + this.getSelf().path());
         }
 		
 	}
